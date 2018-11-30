234654bfa5566b00c0e5a8c2fb4bee391a561bc5
jingzhong <chunyangwanhua>
2018-11-30 09:44:01 +0800

    编写starter

---
+++ src/main/java/com/person/PersonProperties.java
@@ -0,0 +1,34 @@
+package com.person;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.person")
+public class PersonProperties {
+    private String name;
+    private int age;
+    private String sex = "M";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
